15bf06338841ee6681689f15988b49b24d33cd73
sateesh <sateeshmhj1@gmail.com>
2022-03-21 22:20:41 +0545

    navigation feat done

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -3,13 +3,6 @@ import { Formik, Form, Field } from "formik";
 import { Api } from "../../Api";
 import { useNavigate } from "react-router";
 
-// const navigating = () => {
-//   let navigate = useNavigate();
-
-//   navigate("/dashboard");
-// };
-// console.log(navigating);
-
 const loginPageStyle = {
   margin: "100px auto 37px",
   maxWidth: "530px",
@@ -19,90 +12,83 @@ const loginPageStyle = {
   boxShadow: "0px 0px 10px 10px rgba(0,0,0,0.15)",
 };
 
-// let navigate = useNavigate();
+const Basic = () => {
+  let navigate = useNavigate();
+  return (
+    <>
+      <div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={async (values, { setSubmitting }) => {
+            const response = await Api("auth/login", "POST", values);
+            setSubmitting(false);
+            console.log(response);
 
-const Basic = () => (
-  <>
-    <div>
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.email) {
-            errors.email = "Required";
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = "Invalid email address";
-          }
-          return errors;
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
-          // setTimeout(() => {
-          // //   alert(JSON.stringify(values, null, 2));
-          //   console.log(response);
-          // }, 400);
-          const response = await Api("auth/login", "POST", values);
-          setSubmitting(false);
-          console.log(response);
-
-          if (response.success) {
-            // let navigate = useNavigate();
-            // navigate("/dashboard")
-            // navigating
-            alert(response.message);
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form style={loginPageStyle}>
-            <div className="d-flex justify-content-center">
-              <a className="display-3" style={{ textDecoration: "none" }}>
-                Log In
-              </a>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email" style={{ margin: "5px" }}>
-                Email
-              </label>
-              <Field
-                type="text"
-                name="email"
-                className={"form-control"}
-                placeholder="Email"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password" style={{ margin: "5px" }}>
-                Password
-              </label>
-              <Field
-                type="password"
-                name="password"
-                className={"form-control"}
-                placeholder="Password"
-              />
-            </div>
-            <div
-              className="d-flex justify-content-center"
-              style={{ margin: "5px" }}
-            >
-              <button
-                type="submit"
-                className="btn btn-primary"
-                disabled={isSubmitting}
+            if (response.success) {
+              navigate("/dashboard");
+            } else {
+              alert(response.message);
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form style={loginPageStyle}>
+              <div className="d-flex justify-content-center">
+                <a className="display-3" style={{ textDecoration: "none" }}>
+                  Log In
+                </a>
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" style={{ margin: "5px" }}>
+                  Email
+                </label>
+                <Field
+                  type="text"
+                  name="email"
+                  className={"form-control"}
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" style={{ margin: "5px" }}>
+                  Password
+                </label>
+                <Field
+                  type="password"
+                  name="password"
+                  className={"form-control"}
+                  placeholder="Password"
+                />
+              </div>
+              <div
+                className="d-flex justify-content-center"
+                style={{ margin: "5px" }}
               >
-                Login
-              </button>
-            </div>
-            {/* <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button> */}
-          </Form>
-        )}
-      </Formik>
-    </div>
-  </>
-);
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={isSubmitting}
+                >
+                  Login
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
+};
 
 export default Basic;
